49b32ae2f3cd6c44404515fc305835dbfbd42d0c
Dierso88 <erso@inbox.ru>
2022-06-20 18:15:54 +0500

    4.1. Разбор первой программы. [#159033 #314512]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Alina Khalilova");
+        System.out.println("30.08.1988");
+
+    }
+}
