a530dcfbc099c305cac582913409e680afa5621b
Dierso88 <erso@inbox.ru>
2024-01-10 10:16:27 +0300

    6.2. Упорядочить массив. [#4441 #471786]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -1,7 +1,6 @@
 package ru.job4j.array;
 
 public class AlgoArray {
-    @SuppressWarnings("checkstyle:EmptyLineSeparator")
     public static void main(String[] args) {
         int[] array = new int[] {5, 3, 2, 1, 4};
         int temp = array[0];
